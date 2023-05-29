ddd77b3ffe7c7c07b727fb6ffe624c8f31751250
Wagner <wagnerparnoffpereira@gmail.com>
2023-05-29 00:20:04 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const personagensContador = document.getElementById("personagens");
+const luasContador = document.getElementById("luas");
+const planetasContador = document.getElementById("planetas");
+const navesContador = document.getElementById("naves");
+
+preencherContadores();
+preencherTabela();
+
+function preencherContadores() {
+  // personagensContador.innerHTML = swapiGet("people/").count;
+  Promise.all([
+    swapiGet("people/"),
+    swapiGet("vehicles/"),
+    swapiGet("planets/"),
+    swapiGet("starships/")
+  ]).then(function (results) {
+    personagensContador.innerHTML = results[0].data.count;
+    luasContador.innerHTML = results[1].data.count;
+    planetasContador.innerHTML = results[2].data.count;
+    navesContador.innerHTML = results[3].data.count;
+  });
+}
+
+async function preencherTabela() {
+  const response = await swapiGet("films/");
+
+  const tableData = response.data.results;
+  console.log(tableData);
+
+  tableData.forEach((film) => {
+    $("#filmsTable").append(`<tr>
+    <td>${film.title}</td>
+    <td>${moment(film.release_date).format("DD/MM/YYYY")}</td>
+    <td>${film.director}</td>
+    <td>${film.episode_id}</td>
+    </tr>`);
+  });
+}
+
+function swapiGet(param) {
+  return axios.get(`https://swapi.dev/api/${param}`);
+}
